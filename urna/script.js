05e6d3e2d92080a6c08ae594e438d59d35d41f71
Wolaci <wolacijose8@gmail.com>
2022-02-18 20:26:32 -0300

    Fazendo funções corrige, branco e confirma

--- urna/script.js
+++ urna/script.js
@@ -7,10 +7,13 @@ let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
 let numero = '';
+let votoBranco = false;
 
 function comecarEtapa(){
   let etapa = etapas[etapaAtual];
   let numeroHtml = '';
+  numero = ''
+  votoBranco = false;
   for(let i=0; i<etapa.numeros;i++){
     if(i===0){
       numeroHtml += '<div class="numero pisca"></div>';
@@ -45,7 +48,11 @@ function atualizaInferface(){
 
     let fotosHtml = '';
     for(let i in candidato.fotos){
-      fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+      if(candidato.fotos[i].small){
+        fotosHtml += `<div class="d-1-image small"><img src="images/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+      }else{
+        fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+      }
     }
     lateral.innerHTML = fotosHtml;
   }else{
@@ -68,13 +75,39 @@ function clicou(n){
   }
 }
 function branco(){
-
+  // if(numero === ''){
+    numero = '';
+    votoBranco = true;
+    seuVotoPara.style.display = 'block';
+    aviso.style.display = 'block';
+    numeros.innerHTML = '';
+    descricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
+    lateral.innerHTML = '';
+  // }else{
+    // alert("Para Votar em BRANCO")
+  // }
 }
 
 function corrige(){
-
+  comecarEtapa();
 }
 function confirma(){
+  let etapa = etapas[etapaAtual];
+  let votoConfirmado = false;
+  if(votoBranco === true){
+    votoConfirmado = true;
+  }
+  else if(numero.length === etapa.numeros){
+    votoConfirmado = true;
+  }
 
+  if(votoConfirmado){
+    etapaAtual++;
+    if(etapas[etapaAtual] !== undefined){
+      comecarEtapa();
+    }else{
+      console.log("fim");
+    }
+  }
 }
 comecarEtapa();
